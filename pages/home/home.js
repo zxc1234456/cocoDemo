dec6bf44c75f0441655438b7ba25c47ec3a2d3d5
1901rx <1901rx>
2019-07-05 16:50:35 +0800

    shouye

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,70 @@
 // pages/home/home.js
+
+function getUserInfo(e) {
+  console.log(e)
+  app.globalData.userInfo = e.detail.userInfo
+  this.setData({
+    userInfo: e.detail.userInfo,
+    hasUserInfo: true
+  })
+}
+
+//获取应用实例
+const app = getApp();
+
 Page({
-
-  /**
-   * 页面的初始数据
-   */
   data: {
+    imgUrls:[
+      "../../img/banner1.jpg",
+      "../../img/banner2.jpg",
+      "../../img/banner3.jpg",
+      "../../img/banner4.jpg"
+    ],
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
 
+  showmsg(){
+    wx.showToast({
+      title: '功能暂未开放'
+    })
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 加载时先让用户授权登陆
+    getUserInfo(e);
 
+    // 判断userInfo是否有数据
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {
+      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    } else {
+      // 在没有 open-type=getUserInfo 版本的兼容处理
+      wx.getUserInfo({
+        success: res => {
+          app.globalData.userInfo = res.userInfo
+          this.setData({
+            userInfo: res.userInfo,
+            hasUserInfo: true
+          })
+        }
+      })
+    }
   },
 
   /**
